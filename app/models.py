80935a16c18b7a122c5b2bd771c728229a6a975d
Monick <monickhnm@gmail.com>
2018-06-18 22:21:15 -0300

    test

--- app/models.py
+++ app/models.py
@@ -1,14 +1,16 @@
+import re
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MinLengthValidator
 from django.utils import timezone
 from django.conf import settings
+from django.core import validators
 
 class Orientador(models.Model):
-	nome = models.CharField('Nome', max_length=100, unique = True)
+	nome = models.CharField('Nome', max_length=100, unique = True, validators=[validators.RegexValidator(re.compile('^[A-Z, ,´,Á-Ú]+$'),
+            'O nome só pode conter letras', 'invalid')])
 
 	def __str__(self):
 		return self.nome
-
 
 	class Meta:
 		verbose_name='Orientador';
@@ -22,14 +24,16 @@ class Orientador(models.Model):
 class Bolsista(models.Model):
 
 	TIPO_CHOICES = (
-	    (0, 'Voluntário'),
-	    (1, 'Remunerado'),
+	    (0, 'VOLUNTÁRIO'),
+	    (1, 'REMUNERADO'),
 	)
 
 
-	nome = models.CharField('Nome', max_length=100)
-	matricula = models.CharField('Matricula', max_length= 100,unique=True)
-	cartao_rfid = models.CharField('Cartão RFID', max_length=100, null=True)
+	nome = models.CharField('Nome', max_length=100, validators=[validators.RegexValidator(re.compile('^[A-Z, ,´,Á-Ú]+$'),
+            'O nome só pode conter letras', 'invalid')])
+	matricula = models.CharField('Matricula',max_length= 14,unique=True, validators=[MinLengthValidator(14),validators.RegexValidator(re.compile('^[\d]+$'),
+            'A matrícula só pode conter números', 'invalid')])
+	cartao_rfid = models.CharField('Cartão RFID', max_length=100, null=True, unique=True)
 	orientador = models.ForeignKey(Orientador, verbose_name='Orientador', related_name='bolsista_professor', on_delete=models.CASCADE, default=True)
 	tipo_bolsa = models.IntegerField('Tipo', choices=TIPO_CHOICES)
 	carga_horaria_semanal = models.IntegerField('Carga Horaria (semanal)', validators=[MinValueValidator(1)])
@@ -50,6 +54,7 @@ class Bolsista(models.Model):
 class Acesso(models.Model):
 	bolsista = models.ForeignKey(Bolsista, verbose_name='Bolsista', related_name='bolsista_acesso', on_delete=models.CASCADE)
 	data = models.DateField('Data Acesso', null=True, blank=True)
+	atualizado_em = models.DateTimeField('Atualizado em', auto_now=True)
 	hora_entrada = models.TimeField('Entrada', null=True, blank=True)
 	hora_saida = models.TimeField('Saída', null = True, blank=True)
 	total_horas = models.DurationField('Total', null = True, blank = True)
